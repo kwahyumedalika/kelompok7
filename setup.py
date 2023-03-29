6e527e90a6954589fb94eded6fa48772be522c2b
kwahyumedalika <127379613+kwahyumedalika@users.noreply.github.com>
2023-03-30 01:52:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ setup(
   author = 'kelompok7',                   # Type in your name
   author_email = 'medalikamanik00@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/kelompok7',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kwahyumedalika/kelompok7/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['SIMPLE', 'FUNCTION', 'MATH'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
